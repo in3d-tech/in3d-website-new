a4eac43c6988aa3a8262d03cbac65d9fc910f571
Eliya Vahl <eliya@in3d-tech.com>
2024-07-04 16:08:13 +0300

    tilted-test-four

--- src/components/viewableContent/mobile/HomeScreenMobile.jsx
+++ src/components/viewableContent/mobile/HomeScreenMobile.jsx
@@ -348,7 +348,7 @@ function AstroModel({
     }
     if (astroRef.current) {
       // Update rotation based on the tilt values
-      const newRotationY = astroRef.current.rotation.y + tilt.tiltLR * 0.05;
+      const newRotationY = astroRef.current.rotation.y + tilt.tiltLR * 0.002;
       astroRef.current.rotation.y = newRotationY;
     }
   });
