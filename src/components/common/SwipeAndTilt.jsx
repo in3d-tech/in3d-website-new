f9dfa621d11a7550583c273472fa5721f7d1d9ea
Eliya Vahl <eliya@in3d-tech.com>
2024-11-18 14:10:35 +0200

    tilt

--- src/components/common/SwipeAndTilt.jsx
+++ src/components/common/SwipeAndTilt.jsx
@@ -123,7 +123,7 @@ function useDeviceOrientation(onOrientationChange, setCustomMessage) {
 
   // Add a state to store the calibrated orientation
   const [verticalCalibration, setVerticalCalibration] = useState({
-    beta: 57,
+    beta: 55,
     gamma: 0,
   });
   const [permission, setPermission] = useState("default");
